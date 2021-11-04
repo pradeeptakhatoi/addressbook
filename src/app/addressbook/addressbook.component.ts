349254ecfd9bc27d884842f2ca9bed32852c38a4
Pradeepta Khatoi <pradeepta20@gmail.com>
2021-11-04 11:14:27 +0530

    [feat]: sorting enabled

--- src/app/addressbook/addressbook.component.ts
+++ src/app/addressbook/addressbook.component.ts
@@ -1,41 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Address {
+  name: string;
+  location: string;
+  phone: string;
+  created: string;
+}
+
 @Component({
   selector: 'app-addressbook',
   templateUrl: './addressbook.component.html',
   styleUrls: ['./addressbook.component.scss']
 })
 export class AddressbookComponent implements OnInit {
-  public addresses = [
-    {
-      name: "Andy",
-      location: "US",
-      phone: "111",
-      created: "2021-01-01",
-    },
-    {
-      name: "Bob",
-      location: "US",
-      phone: "222",
-      created: "2021-01-01",
-    },
-    {
-      name: "Sandy",
-      location: "US",
-      phone: 333,
-      created: "2021-01-01",
-    },
-    {
-      name: "Jimmy",
-      location: "US",
-      phone: "444",
-      created: "2021-01-01",
-    }
-  ];
+  public addresses: Address[] = [];
   private checkedList: number[] = [];
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.addresses = [
+      {
+        name: "Kanhu",
+        location: "India",
+        phone: "111",
+        created: "2021-01-01",
+      },
+      {
+        name: "Bob",
+        location: "US",
+        phone: "222",
+        created: "2021-01-02",
+      },
+      {
+        name: "Sandy",
+        location: "Canada",
+        phone: "333",
+        created: "2021-01-04",
+      },
+      {
+        name: "Jimmy",
+        location: "Japan",
+        phone: "444",
+        created: "2021-01-03",
+      }
+    ];
+  }
 
   public add() {
     const newAddress = {
@@ -61,6 +70,18 @@ export class AddressbookComponent implements OnInit {
     if (this.checkedList.length > 0) {
       this.addresses = this.addresses.filter((e, i) => !this.checkedList.includes(i));
       this.checkedList = [];
+    }
+  }
+
+  public sort(column: string) {
+    if (column === "name") {
+      this.addresses = this.addresses.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+    } else if (column === "location") {
+      this.addresses = this.addresses.sort((a, b) => (a.location > b.location) ? 1 : ((b.location > a.location) ? -1 : 0));
+    } else if (column === "phone") {
+      this.addresses = this.addresses.sort((a, b) => (a.phone > b.phone) ? 1 : ((b.phone > a.phone) ? -1 : 0));
+    } else if (column === "created") {
+      this.addresses = this.addresses.sort((a, b) => (a.created > b.created) ? 1 : ((b.created > a.created) ? -1 : 0));
     }
   }
 
